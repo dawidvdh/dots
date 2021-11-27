3d6d4ab07972583d30806de223e236af0b8471d3
dawidvdh <hello@dawid.codes>
2021-11-27 15:01:27 +0100

    feat: add more preference

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,9 +1,9 @@
 hs.hotkey.bind({"alt"}, "space", function()
-    local alacritty = hs.application.find('alacritty')
+    local alacritty = hs.application.find("Alacritty")
     local awin = alacritty:mainWindow()
     if awin and alacritty:isFrontmost() then
       alacritty:hide()
     else
-      alacritty = hs.application.launchOrFocus("/Applications/Alacritty.app")
+      alacritty = hs.application.launchOrFocus('/Applications/Alacritty.app')
     end
   end)
